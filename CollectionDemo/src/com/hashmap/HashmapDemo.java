637beb5a530ff4060755db8202706dfe471c22fa
saina <saina@Sainath>
2022-08-09 14:08:52 +0530

    Collectiondemo first commit

---
+++ CollectionDemo/src/com/hashmap/HashmapDemo.java
@@ -0,0 +1,16 @@
+package com.hashmap;
+
+import java.util.HashMap;
+
+public class HashmapDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashMap<Integer,String> hm=new HashMap<Integer,String>();
+		
+
+		
+	}
+
+}
